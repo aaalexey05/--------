1812376a0ee465db68e4bda0aed7678e5deb105e
aaalexey05 <alexeshilyaev@yandex.ru>
2024-06-09 18:22:10 +0700

    upd1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,23 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            
+            for(int i = 1; i < 5; i++){
+                switch(i){
+                    case 1:
+                        System.Console.WriteLine("В ветви case 1");
+                        goto case 3;
+                    case 2:
+                        System.Console.WriteLine("В ветви case 2");
+                        goto case 1;
+                    case 3:
+                        System.Console.WriteLine("В ветви case 3");
+                        goto default;
+                    default:
+                        System.Console.WriteLine("В ветви default");
+                        break;
+                }
+                System.Console.WriteLine();
+            }
 
         }
     }
