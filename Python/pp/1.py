91f8fde44916ad9852073811d05ba82f8cd06777
aalexey05 <alexeshilyaev@yandex.ru>
2024-06-13 16:10:43 +0700

    upd

--- Python/pp/1.py
+++ Python/pp/1.py
@@ -1,8 +1,8 @@
 import moviepy.editor
 from pathlib import Path
 
-videoFile = Path('[Free for profit] 2010 pop x Kesha x Hyperpop type beat - _Price_ (prod. Aki).mp4')
+videoFile = Path('file/[Free for profit] 2010 pop x Kesha x Hyperpop type beat - _Price_ (prod. Aki).mp4')
 
 video = moviepy.editor.VideoFileClip(f'{videoFile}')
 audio = video.audio
-audio.write_audiofile(f'{videoFile.stem}.mp3')
+audio.write_audiofile(f'file/{videoFile.stem}.mp3')
