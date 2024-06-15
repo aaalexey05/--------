baebde3eb587def8716364523bba72c17640bc44
aaalexey05 <alexeshilyaev@yandex.ru>
2024-06-15 20:14:14 +0700

    upd

--- Python/telegram_bot/bot.py
+++ Python/telegram_bot/bot.py
@@ -2,3 +2,6 @@ import telebot
 
 bot = telebot.TeleBot('6865556440:AAGZnh7OkUP-w2gwNlG-PYTlFmslkzFwE5w')
 
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, "Hello!")
