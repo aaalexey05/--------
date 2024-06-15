6f1add67f9cc78a9fb5002e39ff0ac85519a2b46
aaalexey05 <alexeshilyaev@yandex.ru>
2024-06-15 20:21:07 +0700

    upd

--- Python/telegram_bot/bot.py
+++ Python/telegram_bot/bot.py
@@ -5,3 +5,9 @@ bot = telebot.TeleBot('6865556440:AAGZnh7OkUP-w2gwNlG-PYTlFmslkzFwE5w')
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, "Hello!")
+
+@bot.message_handler(command=['help'])
+def help(message):
+    bot.send_message(message.chat.id, "Help information:\n-/start - запуск бота")
+    
+bot.polling(non_stop=True)
