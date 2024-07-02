2823de1997b7db92c716c5ae2f325674c9faebd2
aaalexey05 <alexeshilyaev@yandex.ru>
2024-07-02 15:17:44 +0700

    upd

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,17 +10,48 @@ namespace ConsoleApp1
     {
         static void Main()
         {
-            int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            int sum = 0;
 
-            System.Console.Write("Numbers: ");
-            for(int i = 0; i < numbers.Length; i++)
-            {
-                System.Console.Write(numbers[i] + " ");
-                sum += i;
-            }
-            System.Console.WriteLine($"\nSum = {sum}");
         }
 
+        class Building
+        {
+            public int Floors;  //количество этажей
+            public int Area;  //общая площадь здания
+            public int Occupants;  //количество жильцов
+        }
+
+        class BuildingDemo
+        {
+            static void Maim()
+            {
+                Building house = new Building();
+                Building office = new Building();
+
+                int areaPP;
+
+                house.Occupants = 4;
+                house.Area = 2500;
+                house.Floors = 2;
+
+                office.Occupants = 25;
+                office.Area = 4200;
+                office.Floors = 3;
+
+                areaPP = house.Area / house.Occupants;
+
+                System.Console.WriteLine("Дом имеет:\n " +
+                                        house.Floors + " этажа/n " +
+                                        house.Occupants + " жильца\n " +
+                                        house.Area + " кв. фунтов общей площади,из них\n " +
+                                        areaPP + " приходится на одного человека");
+
+                areaPP = office.Area / office.Occupants;
+                System.Console.WriteLine("Офис имеет:\n " +
+                                        office.Floors + " этажа/n " +
+                                        office.Occupants + " жильца\n " +
+                                        office.Area + " кв. фунтов общей площади,из них\n " +
+                                        areaPP + " приходится на одного человека");
+            }
+        }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0dd95e09f586fd85c7e802c752f02b3cdd6412c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540e2216e22cc7f912c4377ef2e238c5fa1ac0a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
